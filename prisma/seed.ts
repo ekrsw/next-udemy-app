778404a1a8d199453bddf362cb2a7c57f4a478c4
ekrsw <eisuke.koresawa@gmail.com>
2025-12-06 21:21:30 +0900

    prisma init

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/generated/prisma/client"
+import { PrismaClient } from "@prisma/client"
 import * as bcrypt from "bcryptjs"
 
 const prisma = new PrismaClient()
@@ -7,10 +7,45 @@ async function main() {
     // クリーンナップ
     await prisma.post.deleteMany()
     await prisma.user.deleteMany()
+
     const hashedPassword = await bcrypt.hash("password123", 12) // ハッシュ化
-    const dummyImage = ['https://picsum.photos/seed/post1/600/400',
-        'https://picsum.photos/seed/post2/600/400'] // ダミー画像
+    const dummyImages = [
+        'https://picsum.photos/seed/post1/600/400',
+        'https://picsum.photos/seed/post2/600/400'
+    ] // ダミー画像
+    
+    // ユーザー作成
+    const user = await prisma.user.create({
+        data: {
+            email: 'test@example.com',
+            name: 'Test User',
+            password: hashedPassword,
+            posts: {
+                create: [
+                    {
+                        title: '初めてのブログ投稿',
+                        content: 'これは初めてのブログ投稿です。',
+                        topImage: dummyImages[0],
+                        published: true
+                    },{
+                        title: '2番目の投稿',
+                        content: 'これは2番目の投稿です。',
+                        topImage: dummyImages[1],
+                        published: true
+                    }
+                ]
+            }
+        }
+    })
+
+    console.log(`🌱 ユーザーと投稿が作成されました: ${user}`)
 }
 
-main().catch((e)=>{console.error(e) process.exit(1)})
-    .finally(async () => {await prisma.$disconnect()})
+main()
+    .catch((e)=>{
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
