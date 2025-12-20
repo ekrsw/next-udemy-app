245f5e87aa1af174012cafa38f857b989b4f5098
ekrsw <eisuke.koresawa@gmail.com>
2025-12-20 19:14:12 +0900

    LoginForm

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,7 @@
-export default function page() {
+import LoginForm from "@/components/auth/LoginForm";
+
+export default function LoginPage() {
   return (
-    <div>page</div>
+    <LoginForm />
   )
 }
